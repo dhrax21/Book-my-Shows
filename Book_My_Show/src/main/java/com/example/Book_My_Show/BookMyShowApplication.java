fd87c70b35ed344455ff71fba57ec6d2baa715b3
Dheeraj Singh <dhrax21bc@gmail.com>
2026-01-27 15:16:35 +0530

    Add VM host and user secrets

--- Book_My_Show/src/main/java/com/example/Book_My_Show/BookMyShowApplication.java
+++ Book_My_Show/src/main/java/com/example/Book_My_Show/BookMyShowApplication.java
@@ -1,15 +1,12 @@
 package com.example.Book_My_Show;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
 @EnableJpaRepositories("com.example.Book_My_Show.Repository")
-@EnableSwagger2
 public class BookMyShowApplication {
 
 	public static void main(String[] args) {
